7ae7d50a12a69c72fec7457f1a4bc7e90d4d35c1
sshokr1 <73960582+sshokr1@users.noreply.github.com>
2020-11-26 02:11:47 +0200

    Add files via upload

---
+++ Names10.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Names10 {
+	public static void main(String args[]) {
+
+		Scanner userinput = new Scanner(System.in);
+		String[] array;
+		array = new String[9];
+		//String i = 0;
+		System.out.println("Enter 10 Football club to pass");
+		for (int i = 0; i < 10; i++) {
+			array[i] = userinput.next();
+			System.out.println("Welcome coach ");
+			//sum += array[i];
+		
+	}
+
+
+}
+}
