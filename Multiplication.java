bc2b6d8cba55601be8b205d927147b50dc54a7c1
sshokr1 <73960582+sshokr1@users.noreply.github.com>
2020-11-26 02:11:12 +0200

    Add files via upload

---
+++ Multiplication.java
@@ -0,0 +1,17 @@
+
+import java.util.Scanner;
+ 
+public class Multiplication {
+ 
+ public static void main(String[] args) {
+  Scanner in = new Scanner(System.in);
+   
+  System.out.print("enter a number: ");
+  int num1 = in.nextInt();
+   
+  for (int i=0; i< 10; i++){
+   System.out.println(num1 + " x " + (i+1) + " = " + 
+     (num1 * (i+1)));
+  }
+ }
+}
